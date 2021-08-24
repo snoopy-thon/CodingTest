abb48a2908106c732bd1bfb400fda80d9412a566
snoopy-thon <67834202+snoopy-thon@users.noreply.github.com>
2021-08-24 21:53:14 +0900

    Update 그리디 큰 수의 법칙.py

--- 그리디 큰 수의 법칙.py
+++ 그리디 큰 수의 법칙.py
@@ -1,28 +1,28 @@
 # 그리디 알고리즘
 # 3-2. 큰 수의 법칙
 # 내 풀이 : 가장 큰 수가 한개일 때와 여러개일 때의 경우로 나눠서 풀었음.
+# N,M,K를 공백으로 구분하여 입력받기
 N, M, K = map(int, input().split())
+# N개의 수를 공백으로 구분하여 입력받기
 num = list(map(int, input().split()))
-num.sort()
-Max = max(num)
-Max_count = num.count(Max)
-ind = num.index(Max)
-new = num[:ind]
-new_Max = max(new)
+
+num.sort()  # 입력받은 수 정렬
+Max = max(num)  # 가장 큰 수
+Max_count = num.count(Max)  # 가장 큰 수의 개수
+ind = num.index(Max)    # 가장 큰 수의 인덱스
+new = num[:ind]     # 가장 큰 수를 제외한 수의 리스트
+new_Max = max(new)  # 두 번째로 큰 수
+
 result = 0
-print("정렬된 리스트 : ",num)
-print("Max : ",Max)
-print("Max_count : ",Max_count)
-print("두번째로 큰 수 : ",new_Max)
-if Max_count > 1:
+if Max_count > 1:   # 가장 큰 수가 여러개일 경우
     result = Max * K * (M // K) + Max * (M % K)
-else:
+else:   # 가장 큰 수가 한 개일 경우
     result = (Max * K + new_Max)*(M // (K+1)) + Max * (M % (K+1))
 
 print(result)
 
 # 책에 있는 풀이 : 굳이 나처럼 두가지 케이스로 나누지 않고 일반화해서 품.
-# 그 대신 가장 큰 수가 더해지는 횟수와 두 번째로 큰 수가 더해지는 횟수를 나누어서 계산함.
+# 그 대신 가장 큰 수가 더해지는 횟수와 두 번째로 큰 수가 더해지는 횟수로 나누어서 계산함.
 # N,M,K를 공백으로 구분하여 입력받기
 N, M, K = map(int, input().split())
 # N개의 수를 공백으로 구분하여 입력받기
