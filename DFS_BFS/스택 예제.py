e3a22915f25c25699c7f10eae3091095b4e80b2f
snoopy-thon <67834202+snoopy-thon@users.noreply.github.com>
2021-08-29 23:05:14 +0900

    Add files via upload

--- DFS_BFS/스택 예제.py
+++ DFS_BFS/스택 예제.py
@@ -4,6 +4,7 @@
 
 stack = []
 
+# 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
 stack.append(5)
 stack.append(2)
 stack.append(3)
@@ -13,5 +14,5 @@ stack.append(1)
 stack.append(4)
 stack.pop()
 
-print(stack)
-print(stack[::-1])
+print(stack)    # 최하단 원소부터 출력
+print(stack[::-1])  #최상단 원소부터 출력
