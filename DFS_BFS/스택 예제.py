e63a080de7348410ccdc5a896086a3a474e758b4
snoopy-thon <67834202+snoopy-thon@users.noreply.github.com>
2021-08-29 23:02:04 +0900

    Add files via upload

---
+++ DFS_BFS/스택 예제.py
@@ -0,0 +1,17 @@
+# DFS / BFS 알고리즘
+# 꼭 필요한 자료구조 기초
+# 5-1. 스택 예제
+
+stack = []
+
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
+
+print(stack)
+print(stack[::-1])
