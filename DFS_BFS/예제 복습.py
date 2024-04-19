b6598b39901e16e93f78156f1999b4d5861e3c8b
Yujin <tkfkd7038@naver.com>
2024-04-19 15:52:40 +0900

    반복,재귀로 구현한 팩토리얼 출력

--- DFS_BFS/예제 복습.py
+++ DFS_BFS/예제 복습.py
@@ -65,3 +65,6 @@ def factorial_recursive(n):
     if n <= 1 :
         return 1
     return n * factorial_iterative(n-1)
+
+print('반복적으로 구현:', factorial_iterative(5))
+print('재귀적으로 구현:',factorial_recursive(5))
