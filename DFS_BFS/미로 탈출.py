cc9d1b20aa89570aced78976d1a822fd442e31dd
Yujin <tkfkd7038@naver.com>
2024-04-27 13:56:22 +0900

    주석 처리

--- DFS_BFS/미로 탈출.py
+++ DFS_BFS/미로 탈출.py
@@ -1,19 +1,29 @@
 from collections import deque
 
-N, M = map(int, input().split())
+# N, M을 공백으로 구분하여 입력받기
+n, m = map(int, input().split())
+# 2차원 리스트의 맵 정보 입력받기
 graph = []
-for i in range(N):
+for i in range(n):
   graph.append(list(map(int, input())))
 
+# 이동할 네 방향 정의(상, 하, 좌, 우)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# BFS 소스코드 구현
 def bfs(x, y):
+  # 큐(Queue) 구현을 위해 deque 라이브러리 사용
   queue = deque()
   queue.append((x,y))
+  # 큐가 빌 때까지 반복
   while queue:
     x, y = queue.popleft()
+    # 현재 위치에서 네 방향으로의 위치 확인
     for i in range(4):
       nx = x + dx[i]
       ny = y + dy[i]
-      if nx < 0 or ny < 0 or nx >= n or ny >=m:
+      if nx < 0 or ny < 0 or nx >= n or ny >= m:
         continue
       if graph[nx][ny] == 0 :
         continue
