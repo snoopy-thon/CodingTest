a35a60e5ab0d00980b7da454a21e03f7442bd27b
Yujin <tkfkd7038@naver.com>
2024-05-01 23:50:22 +0900

    sheep, wolf 초기화

--- DFS_BFS/양치기꿍.py
+++ DFS_BFS/양치기꿍.py
@@ -4,21 +4,28 @@ graph = []
 for i in range(R):
   graph.append(list(input()))
 
+sheep = 0
+wolf = 0
 
 def dfs(x, y):
   global sheep, wolf
   if x <= -1 or x >= R or y <= -1 or y >= C:
-    return
+    return False
   if graph[x][y] != '#':
     if graph[x][y] == 'v':
       wolf += 1
     elif graph[x][y] == 'k':
       sheep += 1
+    
+    # 해당 노드 방문 처리
     graph[x][y] = '#'
+
+    # 상, 하, 좌, 우의 위치들도 모두 재귀적으로 호출
     dfs(x - 1, y)
     dfs(x, y - 1)
     dfs(x + 1, y)
     dfs(x, y + 1)
+
     return True
   return False
 
@@ -36,6 +43,9 @@ for i in range(R):
       else:
         sh_result -= sheep
         wol_result += wolf
+
+      sheep = 0
+      wolf = 0
 
 
 print("sheep_result:%d wolf_result:%d" %(sh_result, wol_result))
